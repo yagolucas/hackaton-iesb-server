b430018338889f84e8c29399f586ea524991add7
Yago Lucas de Souza Freitas <cast.yago.utic@sebrae.com.br>
2022-03-30 15:42:10 -0300

    criando subrota rota

--- src/model/running.js
+++ src/model/running.js
@@ -25,4 +25,32 @@ router.get("/", async (req, res) => {
   }
 });
 
+const replaceAttr = (obj) => {
+  const retorno = {
+    date: obj.date,
+    values: obj.values.rota,
+  };
+  return retorno;
+}
+
+router.get("/rotas", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("admin");
+    const running = db.collection("RUNNING");
+    await running.find({ }).limit(5).toArray((err, data) => {
+      if(err) {
+        res.status(400).send("recurso não encontrado");
+        return;
+      }
+      const newData = data.map(replaceAttr);
+      res.json(newData);
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({ error: true, message: "erro ao conectar com a base" });
+    client.close();
+  }
+} , []);
+
 module.exports = router;
